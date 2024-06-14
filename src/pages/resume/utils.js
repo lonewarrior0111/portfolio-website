2658cb74710db84144c2212f2f164b2088884324
shubham singh parihar <shubh_parihar@hotmail.com>
2024-06-14 12:56:25 +0530

    completed the skills and the resume pages!!

---
+++ src/pages/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+  experience: [
+    {
+      title: "Backend Developer Intern",
+      subTitle: "Remote",
+      description: "Ioready",
+      date: "Feb, 2024-present",
+    },
+    {
+      title: "Full Stack Developer Intern",
+      subTitle: "Remote",
+      description: "Acciojob",
+      date: "Dec, 2022-present",
+    },
+  ],
+  education: [
+    {
+      title: "MA",
+      //   subTitle: "Lucknow University",
+      description: "Master of Arts In Political Science",
+      date: "July, 2021-August 2023",
+    },
+    {
+      title: "B.Tech",
+      //   subTitle: "Dr.APJ Abdul Kalam Technical University",
+      description: "Mechanical Engineering",
+      date: "2015-2019",
+    },
+    {
+      title: "HSC",
+      //   subTitle: "CMS",
+      description: "12th Grade",
+      date: "2013-2014",
+    },
+    {
+      title: "Matriculation",
+      //   subTitle: "CMS",
+      description: "10th grade",
+      date: "1998-2012",
+    },
+  ],
+};
---
+++ src/pages/skills/utils.js
@@ -0,0 +1,53 @@
+export const skillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      {
+        skillName: "HTML",
+        percentage: "80",
+      },
+      {
+        skillName: "CSS",
+        percentage: "30",
+      },
+      {
+        skillName: "JAVASCRIPT",
+        percentage: "90",
+      },
+    ],
+  },
+  {
+    label: "BACK END",
+    data: [
+      {
+        skillName: "NODE.JS",
+        percentage: "95",
+      },
+      {
+        skillName: "EXPRESS.JS",
+        percentage: "95",
+      },
+      {
+        skillName: "GO",
+        percentage: "35",
+      },
+    ],
+  },
+  {
+    label: "DATABASES",
+    data: [
+      {
+        skillName: "SQL",
+        percentage: "70",
+      },
+      {
+        skillName: "MONGO DB",
+        percentage: "90",
+      },
+      // {
+      //   skillName: "ORACLE",
+      //   percentage: "85",
+      // },
+    ],
+  },
+];
